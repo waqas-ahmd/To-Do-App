f74050da259e234b1377377d7a7ccd15cd0f26f6
waqas-ahmd <65waqas@gmail.com>
2020-11-25 10:10:31 +0500

    Added netlify Identity

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,3 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Container } from "theme-ui";
+import "./index.css";
+import netlifyIdentity from "netlify-identity-widget";
 
-export default (props) => <div>Hello World</div>;
+export default (props) => {
+  useEffect(() => {
+    netlifyIdentity.init({});
+  });
+
+  return (
+    <Container>
+      <div className="d1">
+        <h3>To Do App</h3>
+        <button
+          onClick={() => {
+            netlifyIdentity.open();
+          }}
+        >
+          Login
+        </button>
+      </div>
+    </Container>
+  );
+};
